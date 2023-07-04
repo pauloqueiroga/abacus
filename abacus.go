d25e49822e324562b6f37dbe526b9f076f3a1021
Paulo Queiroga <pqueiroga@wencomine.com>
2023-07-03 17:09:16 -0700

    adding gitlogs command

--- abacus.go
+++ abacus.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -19,6 +22,8 @@ func main() {
 
 	projectsCsvPath := "local-only/projects.csv"
 	pullrequestsCsvPath := "local-only/pullrequests.csv"
+	gitlogCsvPath := "local-only/gitlog.csv"
+	localReposFolder := "local-only/repos"
 
 	switch os.Args[1] {
 	case "projects":
@@ -43,6 +48,28 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	case "gitlogs":
+		if len(os.Args) < 3 {
+			fmt.Println("At least two arguments are required.")
+			printUsage()
+			return
+		}
+		if len(os.Args) >= 4 {
+			pullrequestsCsvPath = os.Args[3]
+		}
+		if len(os.Args) >= 5 {
+			gitlogCsvPath = os.Args[4]
+		}
+		if len(os.Args) >= 6 {
+			localReposFolder = os.Args[5]
+		}
+		err := calculateGitLogStats(os.Args[2], pullrequestsCsvPath, gitlogCsvPath, localReposFolder)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "help":
+		printUsage()
+		return
 	default:
 		fmt.Println("Don't know how to process this command...", os.Args)
 		printUsage()
@@ -178,6 +205,108 @@ func getPullRequests(baseUrl, minDate, maxDate, targetRefName string, output *os
 	return nil
 }
 
+func calculateGitLogStats(baseUrl, pullrequestsCsvPath, gitlogCsvPath, localReposFolder string) error {
+	input, err := os.Open(pullrequestsCsvPath)
+	if err != nil {
+		return err
+	}
+	defer input.Close()
+
+	output, err := os.Create(gitlogCsvPath)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	appendRecord(output,
+		"pullRequestId",
+		"authorUsername",
+		"closedDate",
+		"repository",
+		"lastMergeCommit",
+		"linesAdded",
+		"linesRemoved",
+		"filePath",
+	)
+
+	csvReader := csv.NewReader(input)
+	field, err := parseFields(csvReader)
+	if err != nil {
+		return err
+	}
+
+	for {
+		pr, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		project := url.PathEscape(pr[field["project"]])
+		repository := url.PathEscape(pr[field["repository"]])
+		branch := strings.ReplaceAll(pr[field["targetRefName"]], "refs/heads/", "")
+		gitUrl := fmt.Sprintf("%s/%s/_git/%s", baseUrl, project, repository)
+		err = cloneIfNeeded(gitUrl, localReposFolder, branch)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		err = getGitLog(localReposFolder, repository, pr[field["lastMergeCommit"]], pr[field["pullRequestId"]], pr[field["authorUsername"]], pr[field["closedDate"]], output)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func parseFields(r *csv.Reader) (map[string]int, error) {
+	record, err := r.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]int)
+	for i, v := range record {
+		result[v] = i
+	}
+
+	return result, nil
+}
+
+func cloneIfNeeded(gitUrl, localReposFolder, targetRefName string) error {
+	commandLine := exec.Command("git", "clone", gitUrl, "--branch", targetRefName, "--single-branch")
+	commandLine.Dir = localReposFolder
+	err := commandLine.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getGitLog(localReposFolder, repository, lastMergeCommit, prId, author, closedDate string, output *os.File) error {
+	commandLine := exec.Command("git", "log", lastMergeCommit, "--numstat", "-1", "--format=")
+	commandLine.Dir = fmt.Sprintf("%s/%s", localReposFolder, repository)
+	out, err := commandLine.Output()
+	if err != nil {
+		return err
+	}
+
+	for _, stat := range strings.Split(string(out), "\n") {
+		if stat == "" {
+			continue
+		}
+		record := make([]string, 0)
+		record = append(record, prId, author, closedDate, repository, lastMergeCommit)
+		record = append(record, strings.Split(stat, "\t")...)
+		appendRecord(output, record...)
+	}
+
+	return nil
+}
+
 func newHttpRequest(method string, url string, reqBody io.Reader) (*http.Request, error) {
 	r, err := http.NewRequest(method, url, reqBody)
 	if err != nil {
@@ -209,4 +338,8 @@ func printUsage() {
 	fmt.Println("            Retrieves a list of projects from the given Azure DevOps Organization URL")
 	fmt.Println("  abacus pullrequests <URL to AzDO org> <minimum date> <maximum date> [output CSV path]")
 	fmt.Println("               Retrieves Pull Requests' metadata")
+	fmt.Println("  abacus gitlogs <Base Git URL> [input CSV path] [output CSV path] [local path for Git repositories]")
+	fmt.Println("            Retrieves Git log statistics for the repositories and branches in the input CSV file")
+	fmt.Println("  abacus help")
+	fmt.Println("            Prints this message")
 }
